78eed18b745534aef731e3983becfeb056c331f8
Suhail <suhailansari.sa354@gmail.com>
2026-06-10 00:20:05 +0530

    Prepare project for deployment

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 axiosInstance.interceptors.request.use(
