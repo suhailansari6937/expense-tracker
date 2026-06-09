a8ceeccfdb5b57415e64dd6d7786f7ebbc9a6847
Suhail <suhailansari.sa354@gmail.com>
2026-06-10 00:10:54 +0530

    database configuration

--- backend/src/main/java/com/dev/expense/tracker/security/JwtService.java
+++ backend/src/main/java/com/dev/expense/tracker/security/JwtService.java
@@ -2,17 +2,19 @@ package com.dev.expense.tracker.security;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
+import org.springframework.beans.factory.annotation.Value;
 
 @Service
 public class JwtService {
 
-    private final String SECRET_KEY =
-            "mysecretkeymysecretkeymysecretkey123456";
+    @Value("${jwt.secret}")
+    private String SECRET_KEY;
 
 
 
