58556db0fc8b960fa6bdcd69c98e752ed24be924
Suhail <suhailansari.sa354@gmail.com>
2026-06-10 02:10:45 +0530

    Fix CORS for deployed backend

--- backend/src/main/java/com/dev/expense/tracker/config/SecurityConfig.java
+++ backend/src/main/java/com/dev/expense/tracker/config/SecurityConfig.java
@@ -70,11 +70,17 @@ public class SecurityConfig {
         CorsConfiguration configuration =
                 new CorsConfiguration();
 
-        configuration.setAllowedOrigins(
-                List.of(
-                        "*"
-                )
-        );
+        configuration.setAllowedOrigins(List.of(
+                "http://localhost:5173"
+        ));
+
+        configuration.setAllowedMethods(List.of(
+                "GET",
+                "POST",
+                "PUT",
+                "DELETE",
+                "OPTIONS"
+        ));
 
         configuration.addAllowedHeader("*");
 
